40422bbe2ca461399f1baf9663740c85be4370c9
Everet Rummel <erummel93@gmail.com>
2018-01-19 06:32:25 -0500

    Loop through within-year search results.

--- test.R
+++ test.R
@@ -12,12 +12,33 @@ form = rvest::html_form(session)[[1]] %>%
 
 current_page = rvest::submit_form(session, form)
 
-current_page %<>%
-  rvest::follow_link(i = 2, css = 'body > div:nth-child(1) > table:nth-child(4) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1) > div:nth-child(1) > a:nth-child(3) > img:nth-child(1)')
+end_result = current_page %>%
+  rvest::html_nodes(css = 'td div:nth-child(1) font:nth-child(1) b:nth-child(1)') %>%
+  rvest::html_text()
 
-# Costs
-costs = current_page %>%
-  rvest::html_nodes(css = 'td:nth-child(3) div font') %>%
-  rvest::html_text() %>%
-  as_tibble() %>%
-  filter(value != 'COST')
+current_result = current_page %>%
+  rvest::html_nodes(css = 'td div:nth-child(1) b:nth-child(3)') %>%
+  rvest::html_text()
+
+costs_2 = list()
+
+while (current_result < end_result) {
+  
+  # Costs
+  costs = current_page %>%
+    rvest::html_nodes(css = 'td:nth-child(3) div font') %>%
+    rvest::html_text() %>%
+    as_tibble() %>%
+    filter(value != 'COST')
+  
+  costs_2[length(costs_2) + 1] = costs
+  
+  current_page %<>%
+    rvest::follow_link(i = 4, css = 'body > div:nth-child(1) > table:nth-child(4) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1) > div:nth-child(1) > a:nth-child(3) > img:nth-child(1)')
+  
+  current_result = current_page %>%
+    rvest::html_nodes(css = 'td div:nth-child(1) b:nth-child(3)') %>%
+    rvest::html_text()
+  
+}
+
