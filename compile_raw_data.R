3d2dc811fa6ab8bb23a87b041f76917c732bff41
Everet Rummel <erummel93@gmail.com>
2018-01-21 01:01:29 -0500

    Scrape data & save!

--- compile_raw_data.R
+++ compile_raw_data.R
@@ -4,4 +4,10 @@ source('scrape_page.R')
 
 source('scrape_metrohist.R')
 
-permits = scrape_metrohist(1900, 1986)
+permits = scrape_metrohist(1900, 1942)
+
+saveRDS(object = permits, file = 'data/permits_1900_1942.Rds')
+
+permits_2 = scrape_metrohist(1943, 1986)
+
+saveRDS(object = permits_2, file = 'data/permits_1943_1986.Rds')
