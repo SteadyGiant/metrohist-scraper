6f04419d504c18101af5a5faea7a8c709ec4e100
Everet Rummel <erummel93@gmail.com>
2018-01-21 01:02:13 -0500

    Add notes, combine datasets, save.

--- compile_raw_data.R
+++ compile_raw_data.R
@@ -11,3 +11,9 @@ saveRDS(object = permits, file = 'data/permits_1900_1942.Rds')
 permits_2 = scrape_metrohist(1943, 1986)
 
 saveRDS(object = permits_2, file = 'data/permits_1943_1986.Rds')
+
+# There are way fewer records from 1943-86 than expected. Check on that later.
+
+permits_all = rbind(permits, permits_2)
+
+saveRDS(object = permits_all, file = 'data/permits_1900_1986.Rds')
